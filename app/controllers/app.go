8f98170a603baecd9a8870c99566d99cda1919db
Mattias Granlund <mtsgrd@gmail.com>
2014-10-08 11:51:22 +0300

    Clean up.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -16,36 +16,46 @@ type App struct {
 	*revel.Controller
 }
 
-var yoApiUrl = os.Getenv("YO_API_URL")
-var yoApiToken = os.Getenv("YO_API_TOKEN")
-var googlePlacesApiKey = os.Getenv("GOOGLE_API_KEY")
-
+// Google Places search radius in meters.
 const radius = "50000"
 
-type locationType struct {
-	Lat float64
-	Lng float64
-}
+var (
+	yoApiUrl           = os.Getenv("YO_API_URL")
+	yoApiToken         = os.Getenv("YO_API_TOKEN")
+	googlePlacesApiKey = os.Getenv("GOOGLE_API_KEY")
+)
 
-type searchResultsType []struct {
-	Name    string
-	PlaceId string `json:"place_id"`
-}
+// Data structures for parsing JSON responses.
+type (
+	locationType struct {
+		Lat float64
+		Lng float64
+	}
 
-type searchResponseType struct {
-	Results searchResultsType
-}
+	searchResultsType []struct {
+		Name    string
+		PlaceId string `json:"place_id"`
+	}
 
-type placeResponseType struct {
-	Result placeResultType
-}
+	searchResponseType struct {
+		Results searchResultsType
+	}
 
-type placeResultType struct {
-	Url string
-}
+	placeResponseType struct {
+		Result placeResultType
+	}
 
+	placeResultType struct {
+		Url string
+	}
+)
+
+// Searches the Google Places API.
 func search(query, location string) *searchResponseType {
+
 	apiUrl, _ := url.Parse("https://maps.googleapis.com/maps/api/place/textsearch/json")
+
+	// Add query parameters.
 	params := apiUrl.Query()
 	s := strings.Split(location, ";")
 	lat, lng := s[0], s[1]
@@ -56,7 +66,7 @@ func search(query, location string) *searchResponseType {
 	params.Add("open", "true")
 	apiUrl.RawQuery = params.Encode()
 
-	log.Println("Executing search :", apiUrl.String())
+	revel.INFO.Println("Executing search :", apiUrl.String())
 	resp, err := http.Get(apiUrl.String())
 	defer resp.Body.Close()
 	if err != nil {
@@ -66,15 +76,14 @@ func search(query, location string) *searchResponseType {
 	body, _ := ioutil.ReadAll(resp.Body)
 	var res searchResponseType
 
-	err = json.Unmarshal(body, &res)
-
-	if err != nil {
+	if err = json.Unmarshal(body, &res); err != nil {
 		log.Fatal("Invalid json response.", apiUrl)
 		panic(err)
 	}
 	return &res
 }
 
+// Looks up place information and returns a direct link.
 func getMapUrl(placeId string) string {
 	apiUrl, _ := url.Parse("https://maps.googleapis.com/maps/api/place/details/json")
 	params := apiUrl.Query()
@@ -82,27 +91,26 @@ func getMapUrl(placeId string) string {
 	params.Add("placeid", placeId)
 	apiUrl.RawQuery = params.Encode()
 
-	log.Println("Requesting place details:", apiUrl.String())
+	revel.INFO.Println("Requesting place details:", apiUrl.String())
 	resp, err := http.Get(apiUrl.String())
-	defer resp.Body.Close()
 	if err != nil {
 		log.Fatal("Request failed:", apiUrl)
 	}
+	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	var res placeResponseType
 
-	err = json.Unmarshal(body, &res)
-
-	if err != nil {
+	if err = json.Unmarshal(body, &res); err != nil {
 		log.Fatal("Invalid json response.", apiUrl)
 		panic(err)
 	}
 	return res.Result.Url
 }
 
+// Sends a Yo back to the user who invoked the search.
 func sendYo(username, link string) {
-	log.Println("Sending yo link to:", username, link)
+	revel.INFO.Println("Sending yo link to:", username, link)
 	_, err := http.PostForm(yoApiUrl, url.Values{"api_token": {yoApiToken},
 		"username": {username}, "link": {link}})
 	if err != nil {
@@ -112,10 +120,7 @@ func sendYo(username, link string) {
 }
 
 func (c App) Yo(query string) revel.Result {
-	log.Println("Yo API Token:", yoApiToken)
-	log.Println("Yo API URL:", yoApiUrl)
-	log.Println("Google API key:", googlePlacesApiKey)
-	log.Println(c.Request.URL.String())
+	revel.INFO.Println("Handling request for:", c.Request.URL.String())
 	username := c.Params.Get("username")
 	userLocation := c.Params.Get("location")
 	response := search(query, userLocation)
@@ -128,4 +133,10 @@ func (c App) Yo(query string) revel.Result {
 		return c.RenderText(mapUrl)
 	}
 	return c.RenderText("")
+}
+
+func init() {
+	log.Println("Yo API Token set:", len(yoApiToken) > 0)
+	log.Println("Yo API URL set:", len(yoApiUrl) > 0)
+	log.Println("Google API key set:", len(googlePlacesApiKey) > 0)
 }
